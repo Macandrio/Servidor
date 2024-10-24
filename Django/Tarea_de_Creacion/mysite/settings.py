e9df8f8efe323e80909fe2ccc5fa2fb226475966
Alejandro Gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2024-10-24 10:58:11 +0200

    subida

--- Django/Tarea_de_Creacion/mysite/settings.py
+++ Django/Tarea_de_Creacion/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ejercicio',
     'django_seed',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
+INTERNAL_IPS = ["127.0.0.1"]
+
 
 ROOT_URLCONF = 'mysite.urls'
 
--- Django/Tarea_de_Creacion/urls.py
+++ Django/Tarea_de_Creacion/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
