2a55c78efd1dee785a50955feaaf4614a19a833f
Alejandro Gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2024-10-25 13:40:36 +0200

    subida

--- Django/Tarea_de_Creacion/ejercicio/views.py
+++ Django/Tarea_de_Creacion/ejercicio/views.py
@@ -17,22 +17,27 @@ def dame_proyecto(request):
 
 # 3. Obtener las tareas asociadas al proyecto, ordenadas por fecha de creación descendente
 def tareas_por_proyecto(request, proyecto_id):
-    tareas = Tarea.objects.filter(proyecto=proyecto_id).select_related("proyecto").order_by('-fecha_creacion')
+    tareas = Tarea.objects.filter(proyecto=proyecto_id).select_related("proyecto").order_by('-fecha_creacion').all()
     return render(request, 'Proyecto/tarea_por_proyecto.html', {'tareas': tareas})
 
 # 4. Obtener todos los usuarios que están asignados a una tarea ordenados por la fecha de asignación de la tarea de forma ascendente
 def asignacion_tarea(request, tarea_id):
-    asignaciontarea = AsignacionTarea.objects.filter(tarea = tarea_id).select_related("usuario").select_related("tarea").order_by('fecha_asignacion')
+    asignaciontarea = AsignacionTarea.objects.filter(tarea = tarea_id).select_related("usuario").select_related("tarea").order_by('fecha_asignacion').all()
     return render(request, 'Asignacion/asignacion_tarea.html', {'asignaciontarea': asignaciontarea})
 
 # 5. todas las tareas que tengan un texto en concreto en las observaciones a la hora de asignarlas a un usuario.
 def texto_observaciones(request,texto_observaciones):
-    asignaciontarea = AsignacionTarea.objects.filter(observaciones__icontains = texto_observaciones).select_related("tarea").select_related("usuario")
+    asignaciontarea = AsignacionTarea.objects.filter(observaciones__icontains = texto_observaciones).select_related("tarea").select_related("usuario").all()
     return render(request, 'Asignacion/texto_observacion.html', {'asignaciontarea': asignaciontarea})
 
 # 6. Obtener Todos las tareas que se han creado entre dos años y el estado sea “Completada”.
-def tarea_completada(request,fecha_ini,fecha_final):
-    tarea = Tarea.objects.filter(estado = "Co",
-                                 fecha_inicio = fecha_ini,
-                                 fecha_fin = fecha_final).select_related("proyecto")
-    return render(request, 'Tarea/tarea_completada.html', {'tarea': tarea})
+def tarea_completada(request,fecha_inicio,fecha_final):
+    tareas = Tarea.objects.filter(estado = "Co",
+                                 fecha_creacion__year__gte = fecha_inicio,
+                                 fecha_creacion__year__lte = fecha_final).select_related("proyecto").all()
+    return render(request, 'Tarea/tarea_completada.html', {'tareas': tareas})
+
+# 7. Crear una URL que obtenga el último usuario que ha comentado en una tarea de un proyecto en concreto.
+def usuario_comentario(request):
+    usuario = Usuario.objects.select_related("comentario")
+    usuario = Usuario.objects.order_by("-comentario__fecha_comentario")[:1].get()
